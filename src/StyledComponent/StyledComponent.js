5b427f392fd6e557871b8a448a0a0d377b50a0d8
이상묵 <gogotr@kakao.com>
2021-04-08 00:15:30 +0900

    style-component

---
+++ src/StyledComponent/StyledComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled, { css } from 'styled-component'
+
+const Box = styled.div`
+    width: 5rem;
+    height: 5rem;
+    background: white;
+    border-radius: 10%;
+`;
+
+const StyledComponent = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default StyledComponent;
